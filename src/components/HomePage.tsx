452ac39c407ec2c8acf2eec2c343a46b4c079281
Jonathan Jimson <orbitron.hd.official@gmail.com>
2026-09-04 21:40:27 +0530

    feat: Temporarily use CMS images as hero banners

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,12 @@
 import { HeroBanner } from './HeroBanner';
 import { UpcomingEvent } from './UpcomingEvent';
 import { AboutSection } from './AboutSection';
+import eventsHeaderImg from '../assets/header/events.webp';
 
 export function HomePage() {
   return (
     <main style={{ flex: 1, position: 'relative', zIndex: 1 }}>
-      <HeroBanner />
+      <HeroBanner imageUrl={eventsHeaderImg} />
       <UpcomingEvent />
       <AboutSection />
     </main>
